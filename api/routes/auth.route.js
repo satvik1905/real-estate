66f910184065d82147ab117be38cba97645955ce
Satvik Khopkar <sjkhopkar@gmail.com>
2024-06-19 15:04:39 -0400

    Create sign up API

---
+++ api/routes/auth.route.js
@@ -0,0 +1,8 @@
+import express, { Router } from "express";
+import { signup } from "../controller/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+
+export default router;
