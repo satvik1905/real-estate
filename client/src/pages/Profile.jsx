0707108c237c3bffde61b516ab87eac2e70cf6f3
Satvik Khopkar <sjkhopkar@gmail.com>
2024-06-23 14:26:28 -0400

    Complete Delete listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -142,6 +142,25 @@ export default function Profile() {
       setShowListingsError(true);
     }
   };
+
+  const handleDeleteListing = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -255,7 +274,14 @@ export default function Profile() {
                 <p>{listing.name}</p>
               </Link>
               <div className="flex flex-col text-sm">
-                <button className="text-red-700 uppercase">Delete</button>
+                <button
+                  onClick={() => {
+                    handleDeleteListing(listing._id);
+                  }}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
                 <button className="text-green-700 uppercase">Edit</button>
               </div>
             </div>
