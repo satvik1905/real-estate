ea0ba97986d87895cc614d9f6dd4df5f956bb57b
Satvik Khopkar <sjkhopkar@gmail.com>
2024-06-23 14:44:30 -0400

    Added update listing api routes

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -25,3 +25,50 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+// export const updateListing = async (req, res, next) => {
+//   const listing = await Listing.findById(req.params.id);
+//   if (!listing) {
+//     return next(errorHandler(404, "Listing not found"));
+//   }
+//   if (req.user.id !== listing.userRef) {
+//     return next(errorHandler(401, "You can only delete your own listing"));
+//   }
+//   try {
+//     const updatedListing = Listing.findByIdAndUpdate(req.params.id, req.body, {
+//       new: true,
+//     });
+//     res.status(200).json(updatedListing);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+export const updateListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found"));
+    }
+    if (req.user.id !== listing.userRef.toString()) {
+      return next(errorHandler(401, "You can only update your own listing"));
+    }
+
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true, // Ensures that validations are run on the updated data
+      }
+    );
+
+    if (!updatedListing) {
+      return next(errorHandler(404, "Listing not found after update"));
+    }
+
+    res.status(200).json({ success: true, data: updatedListing });
+  } catch (error) {
+    next(error);
+  }
+};
